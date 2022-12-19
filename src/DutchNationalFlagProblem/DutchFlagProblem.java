80b867a1aa75f7d3a15d37017447578b634a482e
patricksilva1 <patricksilva5160@gmail.com>
2022-12-19 19:46:22 -0300

    Leetcode

--- src/DutchNationalFlagProblem/DutchFlagProblem.java
+++ src/DutchNationalFlagProblem/DutchFlagProblem.java
@@ -1,8 +1,60 @@
+
+
+// Leetcode number 75. Sort Colors 
+// Runtime = 0ms; Memory 40.3 MB; Neats 99.32%
+/*
+ * class Solution {
+    public void sortColors(int[] nums) {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        
+        //1.iterate and count each color
+        for(int num : nums)
+        {
+            switch(num)
+            {
+                case 0:
+                    a+=1;
+                    break;
+                case 1:
+                    b+=1;
+                    break;
+                case 2:
+                    c+=1;
+                    break;
+            }
+        }
+        
+        //2.rewrite array
+        int cnt = 0;
+        while(a>0)
+        {
+            nums[cnt] = 0;
+            a--;
+            cnt++; 
+        }
+        
+        while(b>0)
+        {
+            nums[cnt] = 1;
+            b--;
+            cnt++; 
+        }
+        
+        while(c>0)
+        {
+            nums[cnt] = 2;
+            c--;
+            cnt++; 
+        }
+    }
+}
+ */
 package DutchNationalFlagProblem;
 
 import java.util.Arrays;
 
-// Leetcode number 75. Sort Colors 
 public class DutchFlagProblem {
 
     private int[] nums;
@@ -21,7 +73,7 @@ public class DutchFlagProblem {
 
         // It has O(N) linear running time complexity
         while (j <= k) {
-            
+
             // When item with index j = 0
             if (nums[j] < pivot) {
                 swap(i, j);
---
+++ src/Leetcode Interview Questions/Problem300/Problem345.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String reverseVowels(String s) {
+        
+        String vowels = "aAeEiIoOuU";
+        
+        if (s.length()==0){ return s; }
+       
+        char[] word = s.toCharArray();
+        int last = s.length()-1;
+        int begin = 0, end = last;
+        char tmp;
+
+        while (begin < end){
+            
+            while (begin < last && vowels.indexOf(word[begin])<0){
+                begin++;
+            }
+            while (end > 0 && vowels.indexOf(word[end])<0){
+                end--;
+            }
+            if (begin < end){
+                tmp = word[begin];
+                word[begin] = word[end];
+                word[end] = tmp;
+                begin++;
+                end--;
+            }
+
+        }
+        
+        return new String(word);
+    }
+}
